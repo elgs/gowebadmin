db29fbcdc9e80f086980ad476978e8e094bc6225
Qian Chen <elgs1980@hotmail.com>
2022-08-05 12:13:00 -0700

    Update.

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -11,6 +11,7 @@ customElements.define('gowebadmin-root',
       // derived from LWElement
       async domReady() {
          this.servers = await api.get('/api/servers/');
+         this.json = JSON.stringify(this.servers, null, 2);
          this.update();
       }
 
@@ -38,10 +39,6 @@ customElements.define('gowebadmin-root',
          server.hosts.splice(hostIndex, 1);
       }
 
-      getHostJSON(host, server) {
-         console.log(JSON.stringify(host, null, 2));
-      }
-
       // server
       newServer() {
          this.servers ??= [];
@@ -55,7 +52,8 @@ customElements.define('gowebadmin-root',
       }
 
       async applyServer(server) {
-         const data = await api.post('/api/server/', server);
+         const serverData = this.getServerJSONData(server, true);
+         const data = await api.post('/api/server/', serverData);
          console.log(data);
       }
 
@@ -64,22 +62,57 @@ customElements.define('gowebadmin-root',
          this.servers.splice(serverIndex, 1);
       }
 
-      getServerJSON(server) {
-         console.log(JSON.stringify(server, null, 2));
+      updateServerJSON(server) {
+         if (!server.configEditor) {
+            server.configEditor = ace.edit(server.getDom().querySelector('.server-json'), {
+               mode: 'ace/mode/json',
+               theme: 'ace/theme/chrome',
+               maxLines: Infinity,
+               showPrintMargin: false,
+               showGutter: false,
+               readOnly: true,
+            });
+            server.configEditor.renderer.attachToShadowRoot();
+         }
+         server.configEditor.setValue(JSON.stringify(this.getServerJSONData(server), null, 2), 1);
+         this.updateServersJSON();
+      }
+
+      toggleServerJSON(server) {
+         this.updateServerJSON(server);
+         server.showConfigEditor = !server.showConfigEditor;
       }
 
       // servers
       async applyServers() {
-         const data = await api.post('/api/servers/', this.servers);
+         const serversData = this.servers.map(server => this.getServerJSONData(server));
+         const data = await api.post('/api/servers/', serversData);
          console.log(data);
       }
 
-      getServersJSON() {
-         console.log(JSON.stringify(this.servers, null, 2));
+      updateServersJSON() {
+         if (!this.configEditor) {
+            this.configEditor = ace.edit(this.shadowRoot.querySelector('.servers-json'), {
+               mode: 'ace/mode/json',
+               theme: 'ace/theme/chrome',
+               maxLines: Infinity,
+               showPrintMargin: false,
+               showGutter: false,
+               readOnly: true,
+            });
+            this.configEditor.renderer.attachToShadowRoot();
+         }
+         const serversData = this.servers.map(server => this.getServerJSONData(server));
+         this.configEditor.setValue(JSON.stringify(serversData, null, 2), 1);
+      }
+
+      toggleServersJSON() {
+         this.updateServersJSON();
+         this.showConfigEditor = !this.showConfigEditor;
       }
 
       // toggle host header
-      toggleHostHeader(event, host) {
+      toggleHostHeader(host) {
          const panel = host.getDom().querySelector('.host-panel');
          if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
@@ -88,6 +121,40 @@ customElements.define('gowebadmin-root',
             panel.style.maxHeight = panel.scrollHeight + `px`;
             host.uiActive = true;
          }
+      }
+
+      getServerJSONData(server, withRuntimeId = false) {
+         const retServer = {};
+         if (withRuntimeId) {
+            retServer.runtime_id = server.runtime_id;
+         }
+         retServer.name = server.name;
+         retServer.type = server.type;
+         retServer.listen = server.listen;
+         retServer.disabled = server.disabled;
+         retServer.hosts = server.hosts.map(host => {
+            const retHost = {};
+            if (withRuntimeId) {
+               retHost.runtime_id = host.runtime_id;
+            }
+            retHost.name = host.name;
+            retHost.disabled = host.disabled;
+            if (server.type === 'http') {
+               if (host.https_redirect_port) {
+                  retHost.https_redirect_port = host.https_redirect_port;
+               } else {
+                  retHost.path = host.path;
+                  retHost.disable_dir_listing = host.disable_dir_listing;
+               }
+            } else if (server.type === 'https') {
+               retHost.path = host.path;
+               retHost.cert_path = host.cert_path;
+               retHost.key_path = host.key_path;
+               retHost.disable_dir_listing = host.disable_dir_listing;
+            }
+            return retHost;
+         });
+         return retServer;
       }
    }
 );
